5f46a3eed5d930bae8d079eb483851e8ad1ff62b
mohummodhossain-design <mohummodhossain@gmail.com>
2025-11-01 22:45:00 +0700

    second commit

---
+++ storage/framework/views/bf58493f212485468478278c4db4171c.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH C:\myxamp\htdocs\laravel12os9\osassfolder\m16assos\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/dc3caa66b15dbd4051eb98fe3733c250.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> allam malilk</h1>
+</body>
+</html><?php /**PATH C:\myxamp\htdocs\laravel12os9\osassfolder\m16assos\resources\views/index.blade.php ENDPATH**/ ?>
